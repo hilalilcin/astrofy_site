d35779d17e03a6c35ad13c3bd6f74d3ddd55dca9
hilalilcin <ilcinhilal@gmail.com>
2024-09-15 16:27:21 +0100

    Website is upload.

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Hilal Ilcin | Data Scientist'
+export const SITE_DESCRIPTION = 'Personal website of Hilal Ilcin';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
